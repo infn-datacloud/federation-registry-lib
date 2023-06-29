073e24834f1c46f2e9306fb595571e7958ac706a
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-06-29 11:57:45 +0200

    Use settings

---
+++ app3/config.py
@@ -0,0 +1,47 @@
+from enum import Enum
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import (
+    AnyHttpUrl,
+    BaseSettings,
+    EmailStr,
+    HttpUrl,
+    validator,
+)
+
+
+class Neo4jUriScheme(Enum):
+    NEO4J: str = "neo4j"
+    NEO4JS: str = "neo4j+s"
+    BOLT: str = "bolt"
+    BOLTS: str = "bolt+s"
+
+
+class Settings(BaseSettings):
+    NEO4J_SERVER: str = "localhost:7687"
+    NEO4J_USER: str = "neo4j"
+    NEO4J_PASSWORD: str = "password"
+    NEO4J_URI_SCHEME: Neo4jUriScheme = "bolt"
+    NEOMODEL_DATABSE_URL: Optional[str] = None
+
+    @validator("NEO4J_URI_SCHEME")
+    def get_enum_val(cls, v: Neo4jUriScheme) -> str:
+        return v.value
+
+    @validator("NEOMODEL_DATABSE_URL", pre=True)
+    def assemble_db_connection(
+        cls, v: Optional[str], values: Dict[str, Any]
+    ) -> Any:
+        if isinstance(v, str):
+            return v
+        s = f"{values.get('NEO4J_URI_SCHEME')}://"
+        s += f"{values.get('NEO4J_USER')}:"
+        s += f"{values.get('NEO4J_PASSWORD')}@"
+        s += f"{values.get('NEO4J_SERVER')}"
+        return s
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
--- app3/main.py
+++ app3/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from neomodel import config
 
+from .config import settings
 from .cluster.api.router import router as cluster_router
 from .flavor.api.router import router as flavor_router
 from .identity_provider.api.router import router as identity_provider_router
@@ -17,7 +18,7 @@ from .sla.api.router import router as sla_router
 from .user_group.api.router import router as user_group_router
 
 
-config.DATABASE_URL = "bolt://neo4j:password@localhost:7687"
+config.DATABASE_URL = settings.NEOMODEL_DATABSE_URL
 
 app = FastAPI()
 app.include_router(cluster_router)
