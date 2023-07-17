33f26b13955aecf9eb4cab77b4b91a6b6e93d84e
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-07-17 17:24:29 +0200

    User group in project stores IDP info

--- app/project/schemas_extended.py
+++ app/project/schemas_extended.py
@@ -1,6 +1,9 @@
 from pydantic import Field
 from typing import List, Optional
 
+from app.flavor.schemas import FlavorRead
+from app.identity_provider.schemas import IdentityProviderRead
+from app.image.schemas import ImageRead
 from app.project.schemas import ProjectRead
 from app.provider.schemas import ProviderRead
 from app.quota.schemas import QuotaRead
@@ -9,8 +12,12 @@ from app.sla.schemas import SLARead
 from app.user_group.schemas import UserGroupRead
 
 
+class UserGroupReadExtended(UserGroupRead):
+    identity_provider: IdentityProviderRead
+
+
 class SLAReadExtended(SLARead):
-    user_group: UserGroupRead
+    user_group: UserGroupReadExtended
 
 
 class QuotaReadExtended(QuotaRead):
@@ -18,6 +25,8 @@ class QuotaReadExtended(QuotaRead):
 
 
 class ProjectReadExtended(ProjectRead):
+    flavors: List[FlavorRead] = Field(default_factory=list)
+    images: List[ImageRead] = Field(default_factory=list)
     provider: ProviderRead
     quotas: List[QuotaReadExtended] = Field(default_factory=list)
     sla: Optional[SLAReadExtended] = None
