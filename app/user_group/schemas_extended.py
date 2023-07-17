fa38f2522f9e9725e7a8daeac8904d04d6a73e0e
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-07-17 17:04:31 +0200

    Show provider from project in SLA

--- app/user_group/schemas_extended.py
+++ app/user_group/schemas_extended.py
@@ -1,10 +1,16 @@
 from pydantic import Field
 from typing import List
 
-from app.user_group.schemas import UserGroupRead
 from app.identity_provider.schemas import IdentityProviderRead
 from app.project.schemas import ProjectRead
+from app.provider.schemas import ProviderRead
 from app.sla.schemas import SLARead
+from app.user_group.schemas import UserGroupRead
+
+
+class ProjectReadExtended(ProjectRead):
+    provider: ProviderRead
+
 
 class SLAReadExtended(SLARead):
     project: ProjectRead
