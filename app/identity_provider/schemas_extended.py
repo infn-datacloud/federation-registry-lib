2fd5b7c769da03e88a19970eb82d042e63bc2262
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-07-17 16:55:34 +0200

    Extend Provider when read from IDP

--- app/identity_provider/schemas_extended.py
+++ app/identity_provider/schemas_extended.py
@@ -1,11 +1,16 @@
 from pydantic import Field
 from typing import List
 
+from app.auth_method.schemas import AuthMethodRead
 from app.identity_provider.schemas import IdentityProviderRead
 from app.provider.schemas import ProviderRead
 from app.user_group.schemas import UserGroupRead
 
 
+class ProviderReadExtended(ProviderRead):
+    relationship: AuthMethodRead
+
+
 class IdentityProviderReadExtended(IdentityProviderRead):
     user_groups: List[UserGroupRead] = Field(default_factory=list)
-    providers: List[ProviderRead] = Field(default_factory=list)
+    providers: List[ProviderReadExtended] = Field(default_factory=list)
