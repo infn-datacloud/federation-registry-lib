a9e3476a9d8ce6864a015db634926eb9fcc3fe71
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-07-19 15:02:59 +0200

    SLA Ext schema shows project quotas

--- app/sla/schemas_extended.py
+++ app/sla/schemas_extended.py
@@ -1,5 +1,9 @@
+from pydantic import Field
+from typing import List
+
 from app.identity_provider.schemas import IdentityProviderRead
 from app.project.schemas import ProjectRead
+from app.project.schemas_extended import QuotaReadExtended
 from app.provider.schemas import ProviderRead
 from app.sla.schemas import SLARead
 from app.user_group.schemas import UserGroupRead
@@ -7,6 +11,7 @@ from app.user_group.schemas import UserGroupRead
 
 class ProjectReadExtended(ProjectRead):
     provider: ProviderRead
+    quotas: List[QuotaReadExtended] = Field(default_factory=list)
 
 
 class UserGroupReadExtended(UserGroupRead):
