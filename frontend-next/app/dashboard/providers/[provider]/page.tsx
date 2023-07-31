20892de2f0cf93c5080829fa2100447f25281ed5
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-07-31 13:47:51 +0200

    Rename componets to components

--- frontend-next/app/dashboard/providers/[provider]/page.tsx
+++ frontend-next/app/dashboard/providers/[provider]/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import Loading from "../loading";
 import { getProviders } from "../../_lib/crud";
-import Skeleton from "./_componets/skeleton";
+import Skeleton from "./_components/skeleton";
 
 export default async function Page({
   params,
