38b6bf2e859ba8823d1c2d1ff2c69c86ebe58794
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-09-06 09:51:05 +0200

    Update test.py

--- backend/scripts/test.py
+++ backend/scripts/test.py
@@ -1,33 +1,73 @@
-from glanceclient.client import Client as create_glance_client
-from glanceclient.v2.client import Client as GlanceClient
-from keystoneauth1.identity.v3 import OidcAccessToken
-from keystoneauth1.session import Session
-from keystoneclient.v3.client import Client as KeystoneClient
-from novaclient.client import Client as create_nova_client
-from novaclient.v2.client import Client as NovaClient
+import openstack as opst
 
-# subprocess.Popen(["oidc-token", "infncloud"],
-# stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# if subprocess.
+# import sys
+#
+# openstack.enable_logging(True, stream=sys.stdout)
 
 
-auth = OidcAccessToken(
-    auth_url="https://keystone.recas.ba.infn.it:443",
-    identity_provider="infn-cloud",
-    protocol="openid",
-    access_token="",
-    project_domain_name="default",
-    project_name="infn-cloud",
-)
-sess = Session(auth=auth)
-keystone = KeystoneClient(session=sess)
+conn = opst.connect(cloud="recas-ba")
 
-nova: NovaClient = create_nova_client(version="2", session=sess)
-flavors = nova.flavors.list()
-for flavor in flavors:
-    print(flavor.to_dict())
+flavors = []
+flav_proj = {}
+for flavor in conn.compute.flavors():
+    if not flavor.is_public:
+        projects = conn.compute.get_flavor_access(flavor)
+        flav_proj[flavor.id] = projects
+    data = flavor.to_dict()
+    data["uuid"] = data.pop("id")
+    flavors.append(data)
 
-glance: GlanceClient = create_glance_client(version="2", session=sess)
-images = glance.images.list()
-for image in images:
-    print(image)
+images = []
+imag_proj = {}
+for image in conn.image.images():
+    is_public = True
+    if image.visibility in ["private", "shared"]:
+        imag_proj[image.id] = [image.owner_id]
+        is_public = False
+    if image.visibility == "shared":
+        members = list(conn.image.members(image))
+        for member in members:
+            if member.status == "accepted":
+                imag_proj[image.id].append(member.id)
+    data = image.to_dict()
+    data["uuid"] = data.pop("id")
+    data["is_public"] = is_public
+    data.pop("visibility")
+    images.append(data)
+
+provider = {}
+provider["flavors"] = flavors
+provider["images"] = images
+
+# print(provider)
+# cmdb_url = "http://localhost:8000"
+
+# print(conn.identity.projects())
+# for project in conn.identity.projects():
+#   print(project)
+
+# resp = requests.post(
+#    url=os.path.join(cmdb_url, "api/v1/providers/"),
+#    data=provider,
+#    headers={
+#        "Authorization": "Bearer"
+#    },
+# )
+# if resp.status_code != 201:
+#    raise  # TODO
+# print(resp.json())
+#
+# for service in conn.service_catalog:
+#   print(service)
+# for service in conn.list_services():
+#    print(service)
+
+# for image in conn.image.images():
+# print(image)
+#    print(image.to_dict().keys())
+#    for k,v in image.to_dict().items():
+#        print(f"{k}: {v}")
+# for project in conn.identity.projects():
+#    print(project)
+# for idp in conn.identity.identity_providers():
+#    print(idp)
