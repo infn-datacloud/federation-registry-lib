f0a97734ddc5dfc1e72f778c9ef9af34a7fb5493
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-09-08 10:43:48 +0200

    Add desc to config obj

--- backend/scripts/models/config.py
+++ backend/scripts/models/config.py
@@ -2,26 +2,28 @@ import sys
 from pathlib import Path
 from typing import List, Optional
 
-from pydantic import AnyHttpUrl, BaseModel, root_validator
+from pydantic import AnyHttpUrl, BaseModel, Field, root_validator
 
 external_path = Path.cwd().parent
 sys.path.insert(1, str(external_path))
 
-from app.location.schemas import LocationBase
-from app.provider.schemas import ProviderBase
+from app.location.schemas import LocationCreate
+from app.provider.schemas import ProviderCreate
 
 
 class IDP(BaseModel):
-    name: str
-    protocol: str
-    group_claim: str
-    endpoint: AnyHttpUrl
+    name: str = Field(description="Identity provider name used to authenticate")
+    protocol: str = Field(description="Protocol used to authenticate")
+    group_claim: str = Field(
+        description="Key name from which derive user's authorization"
+    )
+    endpoint: AnyHttpUrl = Field(description="Identity provider endpoint")
 
 
 class Project(BaseModel):
-    id: Optional[str]
-    name: Optional[str]
-    domain: Optional[str]
+    id: Optional[str] = Field(default=None, description="Project unique ID")
+    name: Optional[str] = Field(default=None, description="Project name")
+    domain: Optional[str] = Field(default=None, description="Project domain name")
 
     @root_validator
     def check_id_or_name_and_domain(cls, values):
@@ -35,27 +37,45 @@ class Project(BaseModel):
         return values
 
 
-class Openstack(ProviderBase):
-    auth_url: str
-    location: LocationBase
-    identity_providers: List[IDP]
-    projects: List[Project]
+class Openstack(ProviderCreate):
+    auth_url: str = Field(description="Identity service endpoint")
+    location: LocationCreate = Field(description="Location details")
+    identity_providers: List[IDP] = Field(
+        description="List of supported identity providers"
+    )
+    projects: List[Project] = Field(description="List of projects to inspect")
 
 
 class CMDB(BaseModel):
-    url: AnyHttpUrl
-    api_ver_providers: str = "v1"
-    api_ver_projects: str = "v1"
-    api_ver_locations: str = "v1"
-    api_ver_flavors: str = "v1"
-    api_ver_images: str = "v1"
-    api_ver_identity_providers: str = "v1"
-    api_ver_services: str = "v1"
-    api_ver_slas: str = "v1"
-    api_ver_user_groups: str = "v1"
+    url: AnyHttpUrl = Field(description="CMDB base URL")
+    api_ver_flavors: str = Field(default="v1", description="Flavors API version to use")
+    api_ver_identity_providers: str = Field(
+        default="v1", description="Identity providers API version to use"
+    )
+    api_ver_images: str = Field(default="v1", description="Images API version to use")
+    api_ver_locations: str = Field(
+        default="v1", description="Locations API version to use"
+    )
+    api_ver_projects: str = Field(
+        default="v1", description="Projects API version to use"
+    )
+    api_ver_providers: str = Field(
+        default="v1", description="Providers API version to use"
+    )
+    api_ver_services: str = Field(
+        default="v1", description="Services API version to use"
+    )
+    api_ver_slas: str = Field(default="v1", description="SLAs API version to use")
+    api_ver_user_groups: str = Field(
+        default="v1", description="User groups API version to use"
+    )
 
 
 class Config(BaseModel):
-    cmdb: CMDB
-    oidc_agent_accounts: List[AnyHttpUrl]
-    openstack: List[Openstack]
+    cmdb: CMDB = Field(description="CMDB configuration parameters")
+    oidc_agent_accounts: List[AnyHttpUrl] = Field(
+        description="List of OIDC-Agent supported identity providers endpoints"
+    )
+    openstack: List[Openstack] = Field(
+        description="List of openstack providers to integrate in the CMDB"
+    )
