f00d58b039405bf74e02d898410746ff445592b1
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-09-22 16:24:03 +0200

    Update provider create extended schema

--- backend/app/provider/schemas_extended.py
+++ backend/app/provider/schemas_extended.py
@@ -9,9 +9,10 @@ from app.identity_provider.schemas import (
 )
 from app.image.schemas import ImageCreate
 from app.location.schemas import LocationCreate, LocationRead, LocationReadPublic
+from app.network.schemas import NetworkCreate
 from app.project.schemas import ProjectCreate, ProjectRead
 from app.provider.schemas import ProviderCreate, ProviderRead, ProviderReadPublic
-from app.region.schemas import RegionRead, RegionReadPublic
+from app.region.schemas import RegionCreate, RegionRead, RegionReadPublic
 from app.service.schemas import (
     BlockStorageServiceCreate,
     BlockStorageServiceRead,
@@ -22,9 +23,11 @@ from app.service.schemas import (
     IdentityServiceCreate,
     IdentityServiceRead,
     IdentityServiceReadPublic,
+    NetworkServiceCreate,
     NetworkServiceRead,
     NetworkServiceReadPublic,
 )
+from app.user_group.schemas import UserGroupCreate
 from pydantic import Field
 
 
@@ -106,7 +109,6 @@ class ProviderReadExtendedPublic(ProviderReadPublic):
     )
 
 
-###############################################
 class IdentityProviderCreateExtended(IdentityProviderCreate):
     """Model to extend the Identity Provider data used to create a new instance
     in the DB with the authentication method details."""
@@ -114,28 +116,59 @@ class IdentityProviderCreateExtended(IdentityProviderCreate):
     relationship: AuthMethodCreate = Field(
         description="Authentication method used by the Provider"
     )
+    user_groups: List[UserGroupCreate] = Field(
+        default_factory=list,
+        description="List of user groups belonging to this identity provider",
+    )
+
+
+class ComputeServiceCreateExtended(ComputeServiceCreate):
+    flavors: List[FlavorCreate] = Field(
+        default_factory=list,
+        description="List of flavors accessible through this service",
+    )
+    images: List[ImageCreate] = Field(
+        default_factory=list,
+        description="List of images accessible through this service",
+    )
+
+
+class NetworkServiceCreateExtended(NetworkServiceCreate):
+    networks: List[NetworkCreate] = Field(
+        default_factory=list,
+        description="List of networks accessible through this service",
+    )
+
+
+class RegionCreateExtended(RegionCreate):
+    location: Optional[LocationCreate] = Field(
+        default=None, description="Geographical site"
+    )
+    block_storage_services: List[BlockStorageServiceCreate] = Field(
+        default_factory=list, description="Block storage service"
+    )
+    compute_services: List[ComputeServiceCreateExtended] = Field(
+        default_factory=list, description="Compute service"
+    )
+    identity_services: List[IdentityServiceCreate] = Field(
+        default_factory=list, description="Identity service"
+    )
+    network_services: List[NetworkServiceCreateExtended] = Field(
+        default_factory=list, description="Network service"
+    )
 
 
 class ProviderCreateExtended(ProviderCreate):
     """Model to extend the Provider data used to create a new instance in the
     DB with the lists of related items."""
 
-    location: Optional[LocationCreate] = Field(
-        default=None, description="Provider location."
-    )
-    flavors: List[FlavorCreate] = Field(
-        default_factory=list, description="List of owned Flavors."
+    regions: List[RegionCreateExtended] = Field(
+        default_factory=list, description="Provider regions."
     )
     identity_providers: List[IdentityProviderCreateExtended] = Field(
         default_factory=list,
         description="List of supported identity providers.",
     )
-    images: List[ImageCreate] = Field(
-        default_factory=list, description="List of owned Images."
-    )
     projects: List[ProjectCreate] = Field(
         default_factory=list, description="List of owned Projects."
     )
-    services: List[
-        Union[BlockStorageServiceCreate, IdentityServiceCreate, ComputeServiceCreate]
-    ] = Field(default_factory=list, description="List of hosted Services.")
