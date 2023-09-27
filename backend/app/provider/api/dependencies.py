9254a182df122002ef7c5c25a715ecff06053ff1
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-09-27 15:19:25 +0200

    Refactoring provider dependencies

--- backend/app/provider/api/dependencies.py
+++ backend/app/provider/api/dependencies.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import Union
 
 from app.identity_provider.crud import identity_provider
 from app.location.crud import location
@@ -85,14 +85,17 @@ def validate_new_provider_values(
 
 
 def valid_provider(item: ProviderCreateExtended) -> None:
-    """Validate projects, identity providers and regions."""
+    """Check provider does not already exists and validate identity providers
+    and regions."""
     is_unique_provider(item)
-    valid_identity_provider_list(item.identity_providers)
-    valid_region_list(item.regions)
+    for idp in item.identity_providers:
+        valid_identity_provider(idp)
+    for region in item.regions:
+        valid_region(region)
 
 
-def valid_identity_provider_list(
-    identity_providers: List[IdentityProviderCreateExtended],
+def valid_identity_provider(
+    item: IdentityProviderCreateExtended,
 ) -> None:
     """Check there are no identity providers with the same endpoint, in the
     identity provider list of the received provider. Check that all items have
@@ -110,54 +113,47 @@ def valid_identity_provider_list(
             or DB entity with given endpoint already exists but has
             different attributes.
     """
-    for idp in identity_providers:
-        db_item = identity_provider.get(endpoint=idp.endpoint)
-        if db_item is not None:
-            data = idp.dict(exclude={"relationship", "user_groups"}, exclude_unset=True)
-            for k, v in data.items():
-                if db_item.__getattribute__(k) != v:
-                    msg = f"Identity Provider '{idp.endpoint}' already exists with "
-                    msg += f"different attributes. Received: {data}. Stored: {db_item}"
+    db_item = identity_provider.get(endpoint=item.endpoint)
+    if db_item is not None:
+        data = item.dict(exclude={"relationship", "user_groups"}, exclude_unset=True)
+        for k, v in data.items():
+            if db_item.__getattribute__(k) != v:
+                msg = f"Identity Provider '{item.endpoint}' already exists with "
+                msg += f"different attributes. Received: {data}. Stored: {db_item}"
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
+
+    for group in item.user_groups:
+        for s in group.slas:
+            db_item = sla.get(doc_uuid=s.doc_uuid)
+            if db_item is not None:
+                db_group = db_item.user_group.single()
+                db_idp = db_group.identity_provider.single()
+                if db_group.name != group.name or db_idp.endpoint != item.endpoint:
+                    msg = f"SLA {s.doc_uuid} already used by another group"
                     raise HTTPException(
                         status_code=status.HTTP_400_BAD_REQUEST, detail=msg
                     )
 
-        for group in idp.user_groups:
-            for s in group.slas:
-                db_item = sla.get(doc_uuid=s.doc_uuid)
-                if db_item is not None:
-                    db_group = db_item.user_group.single()
-                    db_idp = db_group.identity_provider.single()
-                    if db_group.name != group.name or db_idp.endpoint != idp.endpoint:
-                        msg = f"SLA {s.doc_uuid} already used by another group"
-                        raise HTTPException(
-                            status_code=status.HTTP_400_BAD_REQUEST, detail=msg
-                        )
 
-
-def valid_region_list(regions: List[RegionCreateExtended]) -> None:
+def valid_region(item: RegionCreateExtended) -> None:
     """"""
-    for region in regions:
-        if region.location is not None:
-            db_item = location.get(site=region.location.site)
-            if db_item is not None:
-                for k, v in region.location.dict(exclude_unset=True).items():
-                    if db_item.__getattribute__(k) != v:
-                        msg = f"Location with site '{region.location.site}' "
-                        msg += "already exists, but with different attributes. "
-                        msg += f"Received: {region.location.dict()}. Stored: {db_item}"
-                        raise HTTPException(
-                            status_code=status.HTTP_400_BAD_REQUEST, detail=msg
-                        )
+    if item.location is not None:
+        db_item = location.get(site=item.location.site)
+        if db_item is not None:
+            for k, v in item.location.dict(exclude_unset=True).items():
+                if db_item.__getattribute__(k) != v:
+                    msg = f"Location '{item.location.site}' already exists with "
+                    msg += f"different attributes. Received: {item.location.dict()}. "
+                    msg += f"Stored: {db_item}"
+                    raise HTTPException(
+                        status_code=status.HTTP_400_BAD_REQUEST, detail=msg
+                    )
 
-        for service in region.block_storage_services:
-            valid_service_endpoint(service)
-
-        for service in region.compute_services:
-            valid_service_endpoint(service)
-
-        for service in region.identity_services:
-            valid_service_endpoint(service)
-
-        for service in region.network_services:
-            valid_service_endpoint(service)
+    for service in item.block_storage_services:
+        valid_service_endpoint(service)
+    for service in item.compute_services:
+        valid_service_endpoint(service)
+    for service in item.identity_services:
+        valid_service_endpoint(service)
+    for service in item.network_services:
+        valid_service_endpoint(service)
