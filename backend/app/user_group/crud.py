b07c46730574bed4c84c83fc3f343728152ae362
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-10-31 13:58:51 +0100

    Update user group CRUD operations

--- backend/app/user_group/crud.py
+++ backend/app/user_group/crud.py
@@ -31,7 +31,7 @@ class CRUDUserGroup(
         UserGroupReadExtendedPublic,
     ]
 ):
-    """"""
+    """User Group Create, Read, Update and Delete operations."""
 
     def create(
         self,
@@ -40,20 +40,35 @@ class CRUDUserGroup(
         identity_provider: IdentityProvider,
         projects: List[Project] = []
     ) -> UserGroup:
+        """Create a new User Group.
+
+        Connect the user group to the given identity provider. Within
+        the provider projects, find the one pointed by the new user
+        group's SLA. If this project already has an SLA, if this SLA has
+        just one project delete it, otherwise, disconnect it from the
+        target project. In any case create (or just update if already
+        exists) the SLA.
+        """
         db_obj = super().create(obj_in=obj_in)
         db_obj.identity_provider.connect(identity_provider)
-        for db_project in projects:
-            if db_project.uuid == obj_in.sla.project:
-                db_sla = db_project.sla.single()
-                if db_sla is not None:
-                    if len(db_sla.projects) == 1:
-                        sla.remove(db_obj=db_sla)
-                    else:
-                        db_project.sla.disconnect(db_sla)
-                sla.create(obj_in=obj_in.sla, user_group=db_obj, project=db_project)
+        db_project = next(
+            filter(lambda x: x.uuid == obj_in.sla.project, projects), None
+        )
+        if db_project:
+            db_sla = db_project.sla.single()
+            if db_sla is not None:
+                if len(db_sla.projects) == 1:
+                    sla.remove(db_obj=db_sla)
+                else:
+                    db_project.sla.disconnect(db_sla)
+            sla.create(obj_in=obj_in.sla, user_group=db_obj, project=db_project)
         return db_obj
 
     def remove(self, *, db_obj: UserGroup) -> bool:
+        """Delete an existing user group and all its relationships.
+
+        At first delete its SLAs. Finally delete the user group.
+        """
         for item in db_obj.slas:
             sla.remove(db_obj=item)
         return super().remove(db_obj=db_obj)
@@ -66,6 +81,12 @@ class CRUDUserGroup(
         projects: List[Project] = [],
         force: bool = False
     ) -> Optional[UserGroup]:
+        """Update User Group attributes.
+
+        By default do not update relationships or default values. If
+        force is True, update linked SLAs and apply default values when
+        explicit.
+        """
         edit = False
         if force:
             edit = self.__update_slas(
@@ -85,25 +106,46 @@ class CRUDUserGroup(
         db_obj: UserGroup,
         provider_projects: List[Project]
     ) -> bool:
+        """Update user group linked SLAs.
+
+        Connect new SLA not already connect, leave untouched already
+        linked ones. Delete old ones no more connected to the user group
+        and pointing only to a project in this provider. If there are
+        SLAs pointing to a project in this provider but also to projects
+        of other providers, disconnect them.
+        """
         edit = False
-        db_items = {db_item.doc_uuid: db_item for db_item in db_obj.slas}
-        db_projects = list(
-            filter(lambda x: x.uuid in obj_in.sla.project, provider_projects)
+        db_project = next(
+            filter(lambda x: x.uuid in obj_in.sla.project, provider_projects), None
         )
-        db_project = db_projects[0]
-        db_item = db_items.pop(obj_in.sla.doc_uuid, None)
-        if not db_item:
-            sla.create(obj_in=obj_in.sla, project=db_project, user_group=db_obj)
-            edit = True
-        else:
-            updated_data = sla.update(
-                db_obj=db_item, obj_in=obj_in.sla, projects=db_projects, force=True
-            )
-            if not edit and updated_data is not None:
+        db_sla_target_provider = next(
+            filter(
+                lambda x: any([p in provider_projects for p in x.projects]), db_obj.slas
+            ),
+            None,
+        )
+
+        # An SLA pointing to this provider already exists and it's document uuid
+        # differs from the new one: Disconnect or delete it.
+        if db_sla_target_provider:
+            if db_sla_target_provider.doc_uuid != obj_in.sla.doc_uuid:
+                if len(db_sla_target_provider.projects) == 1:
+                    sla.remove(db_obj=db_sla_target_provider)
+                else:
+                    db_sla_target_provider.user_group.disconnect(db_obj)
+                sla.create(obj_in=obj_in.sla, project=db_project, user_group=db_obj)
                 edit = True
-        # for db_item in db_items.values():
-        #     sla.remove(db_obj=db_item)
-        #     edit = True
+            else:
+                updated_data = sla.update(
+                    db_obj=db_sla_target_provider,
+                    obj_in=obj_in.sla,
+                    projects=provider_projects,
+                    force=True,
+                )
+                if not edit and updated_data is not None:
+                    edit = True
+        else:
+            sla.create(obj_in=obj_in.sla, project=db_project, user_group=db_obj)
         return edit
 
 
