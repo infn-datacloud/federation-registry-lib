79a3dae24597d0d491104e727507c4d772441a05
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2023-12-14 09:57:17 +0100

    Add provider fixtures with relationships and duplicate items

--- tests/provider/fixtures.py
+++ tests/provider/fixtures.py
@@ -1,5 +1,6 @@
 """Provider specific fixtures."""
-from typing import Any, Dict, Tuple, Type, Union
+from typing import Any, Dict, List, Tuple, Type, Union
+from uuid import uuid4
 
 from pytest_cases import fixture, fixture_ref, parametrize
 
@@ -13,9 +14,15 @@ from app.provider.schemas import (
     ProviderUpdate,
 )
 from app.provider.schemas_extended import (
+    AuthMethodCreate,
+    IdentityProviderCreateExtended,
+    ProjectCreate,
     ProviderCreateExtended,
     ProviderReadExtended,
     ProviderReadExtendedPublic,
+    RegionCreateExtended,
+    SLACreateExtended,
+    UserGroupCreateExtended,
 )
 from tests.common.schema_validators import (
     BaseSchemaValidation,
@@ -23,7 +30,13 @@ from tests.common.schema_validators import (
     ReadSchemaValidation,
 )
 from tests.utils.provider import random_status, random_type
-from tests.utils.utils import random_bool, random_email, random_lower_string
+from tests.utils.utils import (
+    random_bool,
+    random_date,
+    random_email,
+    random_lower_string,
+    random_url,
+)
 
 invalid_create_key_values = {
     ("description", None),
@@ -122,19 +135,21 @@ def provider_read_class(cls) -> Any:
 
 
 @fixture
-def provider_mandatory_data() -> Dict[str, Any]:
+def provider_create_mandatory_data() -> Dict[str, Any]:
     """Dict with Provider mandatory attributes."""
     return {"name": random_lower_string(), "type": random_type()}
 
 
 @fixture
-def provider_all_data(provider_mandatory_data: Dict[str, Any]) -> Dict[str, Any]:
+def provider_create_all_data(
+    provider_create_mandatory_data: Dict[str, Any],
+) -> Dict[str, Any]:
     """Dict with all Provider attributes.
 
     Attribute is_public has been parametrized.
     """
     return {
-        **provider_mandatory_data,
+        **provider_create_mandatory_data,
         "status": random_status(),
         "is_public": random_bool(),
         "support_emails": [random_email()],
@@ -142,7 +157,84 @@ def provider_all_data(provider_mandatory_data: Dict[str, Any]) -> Dict[str, Any]
 
 
 @fixture
-@parametrize("data", {fixture_ref("provider_mandatory_data")})
+@parametrize(owned_regions=relationships_num)
+def provider_create_data_with_regions(
+    owned_regions: int,
+    provider_create_all_data: Dict[str, Any],
+) -> Dict[str, Any]:
+    """Dict with all Provider attributes and regions."""
+    regions = []
+    for _ in range(owned_regions):
+        regions.append(RegionCreateExtended(name=random_lower_string()))
+    return {**provider_create_all_data, "regions": regions}
+
+
+@fixture
+@parametrize(
+    owned_projects=relationships_num,
+)
+def provider_create_data_with_projects(
+    owned_projects: int,
+    provider_create_all_data: Dict[str, Any],
+) -> Dict[str, Any]:
+    """Dict with all Provider attributes and regions."""
+    projects = []
+    for _ in range(owned_projects):
+        projects.append(ProjectCreate(name=random_lower_string(), uuid=uuid4()))
+    return {**provider_create_all_data, "projects": projects}
+
+
+@fixture
+def provider_create_data_with_idps(
+    provider_create_data_with_projects: Dict[str, Any],
+) -> Dict[str, Any]:
+    """Dict with all Provider attributes and regions."""
+    projects: List[ProjectCreate] = provider_create_data_with_projects.get("projects")
+    identity_providers = []
+    for i in range(len(projects)):
+        d1 = random_date()
+        d2 = random_date()
+        if d1 < d2:
+            start_date = d1
+            end_date = d2
+        else:
+            start_date = d2
+            end_date = d1
+        identity_providers.append(
+            IdentityProviderCreateExtended(
+                endpoint=random_url(),
+                group_claim=random_lower_string(),
+                relationship=AuthMethodCreate(
+                    idp_name=random_lower_string(), protocol=random_lower_string()
+                ),
+                user_groups=[
+                    UserGroupCreateExtended(
+                        name=random_lower_string(),
+                        sla=SLACreateExtended(
+                            doc_uuid=uuid4(),
+                            start_date=start_date,
+                            end_date=end_date,
+                            project=projects[i].uuid,
+                        ),
+                    )
+                ],
+            )
+        )
+    return {
+        **provider_create_data_with_projects,
+        "identity_providers": identity_providers,
+    }
+
+
+@fixture
+@parametrize(
+    "data",
+    {
+        fixture_ref("provider_create_mandatory_data"),
+        fixture_ref("provider_create_data_with_regions"),
+        fixture_ref("provider_create_data_with_idps"),  # Include case with projects
+    },
+)
 def provider_create_valid_data(data: Dict[str, Any]) -> Dict[str, Any]:
     """Valid set of attributes for a Provider patch schema."""
     return data
@@ -151,20 +243,98 @@ def provider_create_valid_data(data: Dict[str, Any]) -> Dict[str, Any]:
 @fixture
 @parametrize("k, v", invalid_create_key_values)
 def provider_create_invalid_pair(
-    provider_mandatory_data: Dict[str, Any], k: str, v: Any
+    provider_create_mandatory_data: Dict[str, Any], k: str, v: Any
 ) -> Dict[str, Any]:
     """Dict with one invalid key-value pair."""
-    data = {**provider_mandatory_data}
+    data = {**provider_create_mandatory_data}
     data[k] = v
     return data
 
 
-# TODO Add fixtures for duplicated values or complex relations raising ValidationError
-# (see at the validator function of ProviderCreateExtended)
+@fixture
+def provider_create_duplicate_regions(
+    provider_create_all_data: Dict[str, Any],
+    region_create_mandatory_data: Dict[str, Any],
+) -> Dict[str, Any]:
+    """Dict with all Provider attributes and regions."""
+    region = RegionCreateExtended(**region_create_mandatory_data)
+    return {**provider_create_all_data, "regions": [region, region]}
 
 
 @fixture
-@parametrize("data", {fixture_ref("provider_create_invalid_pair")})
+def provider_create_duplicate_projects(
+    provider_create_all_data: Dict[str, Any],
+    project_create_mandatory_data: Dict[str, Any],
+) -> Dict[str, Any]:
+    """Dict with all Provider attributes and regions."""
+    project = ProjectCreate(**project_create_mandatory_data)
+    return {**provider_create_all_data, "projects": [project, project]}
+
+
+@fixture
+def provider_create_duplicate_idps(
+    provider_create_all_data: Dict[str, Any],
+    identity_provider_create_mandatory_data: Dict[str, Any],
+) -> Dict[str, Any]:
+    """Dict with all Provider attributes and regions."""
+    projects = [
+        ProjectCreate(name=random_lower_string(), uuid=uuid4()),
+        ProjectCreate(name=random_lower_string(), uuid=uuid4()),
+    ]
+    identity_providers = []
+    for i in range(len(projects)):
+        d1 = random_date()
+        d2 = random_date()
+        if d1 < d2:
+            start_date = d1
+            end_date = d2
+        else:
+            start_date = d2
+            end_date = d1
+        identity_providers.append(
+            IdentityProviderCreateExtended(
+                **identity_provider_create_mandatory_data,
+                relationship=AuthMethodCreate(
+                    idp_name=random_lower_string(), protocol=random_lower_string()
+                ),
+                user_groups=[
+                    UserGroupCreateExtended(
+                        name=random_lower_string(),
+                        sla=SLACreateExtended(
+                            doc_uuid=uuid4(),
+                            start_date=start_date,
+                            end_date=end_date,
+                            project=projects[i].uuid,
+                        ),
+                    )
+                ],
+            )
+        )
+    return {
+        **provider_create_all_data,
+        "projects": projects,
+        "identity_providers": identity_providers,
+    }
+
+
+# TODO Add fixtures for invalid create cases:
+# (see at the validator function of ProviderCreateExtended)
+# - check_idp_projs_exits
+# - check_block_storage_serv_projs_exist
+# - check_compute_serv_projs_exist
+# - check_network_serv_projs_exist
+
+
+@fixture
+@parametrize(
+    "data",
+    {
+        fixture_ref("provider_create_invalid_pair"),
+        fixture_ref("provider_create_duplicate_regions"),
+        fixture_ref("provider_create_duplicate_projects"),
+        fixture_ref("provider_create_duplicate_idps"),
+    },
+)
 def provider_create_invalid_data(data: Dict[str, Any]) -> Dict[str, Any]:
     """Invalid set of attributes for a Provider create schema."""
     return data
@@ -228,9 +398,9 @@ def provider_patch_invalid_data(data: Dict[str, Any]) -> Dict[str, Any]:
 
 
 @fixture
-def db_provider_simple(provider_mandatory_data: Dict[str, Any]) -> Provider:
+def db_provider_simple(provider_create_mandatory_data: Dict[str, Any]) -> Provider:
     """Fixture with standard DB Provider."""
-    item = ProviderCreateExtended(**provider_mandatory_data)
+    item = ProviderCreateExtended(**provider_create_mandatory_data)
     return provider_mng.create(obj_in=item)
 
 
