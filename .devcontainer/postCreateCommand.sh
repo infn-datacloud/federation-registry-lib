204b04982d589c8959a62cc11f4422e8db1f9784
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-03-20 08:51:11 +0000

    Update devcontainer settings and startup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,4 @@
-poetry update
+poetry install
 
 echo alias oidc-add=\'docker exec -it \${OIDC_AGENT_CONTAINER_NAME} oidc-add\' >> /home/node/.bashrc
 echo alias oidc-gen=\'docker exec -it \${OIDC_AGENT_CONTAINER_NAME} oidc-gen\' >> /home/node/.bashrc
