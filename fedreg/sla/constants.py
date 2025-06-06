99d7665293da47caabe991fab20a1bf1f30837c8
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-06-06 09:11:05 +0000

    chore!: remove SLA entity

--- fedreg/sla/constants.py
+++
@@ -1,7 +0,0 @@
-"""Definition of SLA constants."""
-
-DOC_UUID = "Unique ID of the document with the SLA details."
-DOC_START = "SLA validity start date."
-DOC_END = "SLA validity end date."
-DOC_EXT_GROUP = "Target user group."
-DOC_EXT_PROJ = "Target projects."
