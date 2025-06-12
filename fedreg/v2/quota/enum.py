3efc73d0447d97e64ebdebb04cb681df458e38e6
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-06-12 13:01:07 +0000

    fix: Enum can't inherits from each others

--- fedreg/v2/quota/enum.py
+++ fedreg/v2/quota/enum.py
@@ -1,7 +1,15 @@
 """Quota enumerations"""
 
+from enum import Enum
+
 from fedreg.v2.service.enum import ServiceType
 
 
-class QuotaType(ServiceType):
+class QuotaType(str, Enum):
+    """Possible Quota types."""
+
+    BLOCK_STORAGE = ServiceType.BLOCK_STORAGE.value
+    COMPUTE = ServiceType.COMPUTE.value
+    NETWORKING = ServiceType.NETWORKING.value
+    OBJECT_STORE = ServiceType.OBJECT_STORE.value
     STORAGECLASS = "storageclass"
