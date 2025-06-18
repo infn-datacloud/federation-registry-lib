e0443bbb218d4fbe246f506d8596c8396caa3102
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-06-18 09:30:29 +0000

    chore: add k8s values to service name enums

--- fedreg/v1/service/enum.py
+++ fedreg/v1/service/enum.py
@@ -7,18 +7,21 @@ class BlockStorageServiceName(str, Enum):
     """Possible IaaS block storage services names."""
 
     OPENSTACK_CINDER = "org.openstack.cinder"
+    KUBERNETES = "storage.k8s.io/v1"
 
 
 class ComputeServiceName(str, Enum):
     """Possible IaaS compute services names."""
 
     OPENSTACK_NOVA = "org.openstack.nova"
+    KUBERNETES = "v1"
 
 
 class IdentityServiceName(str, Enum):
     """Possible IaaS identity services names."""
 
     OPENSTACK_KEYSTONE = "org.openstack.keystone"
+    KUBERNETES = "v1"
 
 
 class NetworkServiceName(str, Enum):
